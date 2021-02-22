22f187c52da373527e3bba4bcab286696a49593d
Luiz Gustavo <gugamatias7@gmail.com>
2021-02-22 17:40:09 -0300

    fix: movendo os codigos de Questoes do URI para fora da pasta src

--- Aula-4/exercicios/main.hs
+++ Aula-4/exercicios/main.hs
@@ -1,8 +1,15 @@
 module Main where
     import Ex1
     import Ex2
+    import Ex3
 
     main :: IO ()
     main = do
         print ex1
-        print (grid 1 2) 
+        -- Output: 338350
+
+        print $ grid 1 2
+        -- Output: [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2)]
+        
+        print $ quadrado 2
+        -- Output: [(0,1),(0,2),(1,0),(1,2),(2,0),(2,1)]
