9b8539091ff38c074b4e3e28801e3512f21b2e5d
Luiz Gustavo <luiz.matias@upe.br>
2021-03-03 08:42:11 -0300

    feat: Finalizada questoes da 'Lista de Exercícios da Semana 4' BREAKING_CHANGE:

--- Aula-6/Ex10.hs
+++ Aula-6/Ex10.hs
@@ -7,6 +7,19 @@ intersection (x:xs) ys = if x `elem` ys
                  then x : intersection xs (delete x ys)
                  else intersection xs ys
 
+intersection' :: Eq a => [a] -> [a] -> [a]
+intersection' xs ys = [ k | k <- xs, k `elem` ys ]
+
+intersection'' :: Eq a => [a] -> [a] -> [a]
+intersection'' xs ys = filter (`elem` ys) xs
+
+
+intersection''' :: Eq a => [a] -> [a] -> [a]
+intersection''' xs ys = [x | x <- xs, y <- ys, x == y]
+
 main :: IO ()
 main = do
     print $ intersection [3,6,5,7] [9,7,5,1,3]
+    print $ intersection' [3,6,5,7] [9,7,5,1,3]
+    print $ intersection'' [3,6,5,7] [9,7,5,1,3]
+    print $ intersection''' [3,6,5,7] [9,7,5,1,3]
--- Aula-6/Ex7.hs
+++ Aula-6/Ex7.hs
@@ -1,9 +1,10 @@
 -- Não finalizada
 repeat' :: Int -> [[Int]]
 repeat' 0 = []
-repeat' n = [1..n] : repeat' (n - 1)
+repeat' n = map (\x -> n) [1..n] : repeat' (n - 1)
 
 
 main :: IO ()
 main = do
     print $ repeat' 4
+    print $ concat $ repeat' 4
